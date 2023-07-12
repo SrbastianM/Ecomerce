9917b4d8746b75485d48e336a160dd04c30e9677
Tenki <srodriguezdev.l@gmail.com>
2023-07-12 13:16:16 -0500

    Se genero standart de el texto pequeño a usar dentro de la app

---
+++ lib/widgets/small_text.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SmallText extends StatelessWidget {
+  final String text;
+  Color? color;
+  double size;
+  double height;
+  SmallText(
+      {super.key,
+      required this.text,
+      this.color = const Color(0xFFccc7c5),
+      this.size = 14,
+      this.height = 1.2});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontFamily: 'Roboto',
+        color: color,
+        fontSize: size,
+        height: height,
+      ),
+    );
+  }
+}
