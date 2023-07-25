ca1275d363681ef4fcdcb44bf2bf63ed5282dea2
Tenki <srodriguezdev.l@gmail.com>
2023-07-25 11:19:27 -0500

    Enterprise computer changes

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,3 +1,6 @@
+import 'package:eccommerce/utils/app_colors.dart';
+import 'package:eccommerce/widgets/big_text.dart';
+import 'package:eccommerce/widgets/small_text.dart';
 import 'package:flutter/material.dart';
 
 class FoodPageBody extends StatefulWidget {
@@ -8,11 +11,15 @@ class FoodPageBody extends StatefulWidget {
 }
 
 class _FoodPageBodyState extends State<FoodPageBody> {
+  //make the next or previous slide visible
+  PageController pageController = PageController(viewportFraction: 0.85);
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 320,
       child: PageView.builder(
+        controller: pageController,
         itemCount: 5,
         itemBuilder: ((context, index) {
           return _buildPageItem(index);
@@ -22,13 +29,72 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 10, right: 10),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: AssetImage(
+                "asset/image/food2.jpg",
+              ),
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Croassaint"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "4.5"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "1287"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "comments")
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
