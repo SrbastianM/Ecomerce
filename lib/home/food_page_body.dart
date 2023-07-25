90b4055c64ed61eb6dd19b502051cd02b54d606b
Tenki <srodriguezdev.l@gmail.com>
2023-07-25 11:38:09 -0500

    First commit

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -27,13 +27,74 @@ class _FoodPageBodyState extends State<FoodPageBody> {
   }
 
   Widget _buildPageItem(int index) {
-    return Container(
-      height: 220,
-      margin: EdgeInsets.only(left: 5, right: 5),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(30),
-        color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
-      ),
+    return Stack(
+      children: [
+        Container(
+          height: 220,
+          margin: EdgeInsets.only(left: 5, right: 5),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
+            image: const DecorationImage(
+                fit: BoxFit.cover, image: AssetImage("asset/images/s.png")),
+          ),
+        ),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Container(
+            height: 140,
+            margin: EdgeInsets.only(left: 25, right: 25, bottom: 15),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Container(
+              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  BigText(text: "Testing text above"),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Wrap(
+                        children: List.generate(
+                          5,
+                          (index) => Icon(
+                            Icons.star,
+                            color: AppColors.mainColor,
+                            size: 15,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "428"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "1202"),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      SmallText(text: "Comments"),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
