41476547bbee279ba710a5694d5c3e1cd38790f5
Tenki <srodriguezdev.l@gmail.com>
2023-07-27 14:43:06 -0500

    Se adicionaron mas constantes para los iconos, textos, paddings y widths

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -10,12 +10,20 @@ class Dimensions {
   static double height10 = screeHeight / 89.03;
   static double height15 = screeHeight / 59.3;
   static double height20 = screeHeight / 44.5;
+  static double height30 = screeHeight / 29.7;
+  static double height45 = screeHeight / 19.7;
   // dynamic width, margin y padding
   static double width10 = screeHeight / 89.03;
   static double width15 = screeHeight / 59.3;
   static double width20 = screeHeight / 44.5;
+  static double width30 = screeHeight / 29.7;
+  static double width45 = screeHeight / 19.7;
 
   static double font20 = screeHeight / 44.5;
+  static double radius15 = screeHeight / 59.3;
   static double radius20 = screeHeight / 44.5;
   static double radius30 = screeHeight / 29.7;
+
+  //Icon Size
+  static double iconSize24 = screeHeight / 37;
 }
