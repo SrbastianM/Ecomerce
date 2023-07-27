13b61d66cfc9ae29bf701168f154edad32e07383
Tenki <srodriguezdev.l@gmail.com>
2023-07-27 17:45:26 -0500

    Se ajustan las dimenciones dinamicas del ListViewbuilder

--- lib/utils/dimensions.dart
+++ lib/utils/dimensions.dart
@@ -12,6 +12,7 @@ class Dimensions {
   static double height20 = screeHeight / 44.5;
   static double height30 = screeHeight / 29.7;
   static double height45 = screeHeight / 19.7;
+
   // dynamic width, margin y padding
   static double width10 = screeHeight / 89.03;
   static double width15 = screeHeight / 59.3;
@@ -26,4 +27,8 @@ class Dimensions {
 
   //Icon Size
   static double iconSize24 = screeHeight / 37;
+
+  // List View Size
+  static double listViewImg = screenWidth / 3.7;
+  static double listViewTextContSize = screenWidth / 4.4;
 }
