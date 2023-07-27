252228a4f4b66776fb68fa2d8cf888f396799e87
Tenki <srodriguezdev.l@gmail.com>
2023-07-27 17:51:53 -0500

    Se elimina un container innecesario y se habilita una propiedad para que no crashee

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -94,93 +94,89 @@ class _FoodPageBodyState extends State<FoodPageBody> {
           ),
         ),
         //Recycler view List
-        Container(
-          height: 700,
-          child: ListView.builder(
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: 10,
-            itemBuilder: (context, index) {
-              return Container(
-                margin: EdgeInsets.only(
-                  left: Dimensions.width20,
-                  right: Dimensions.width20,
-                  bottom: Dimensions.height10,
-                ),
-                child: Row(
-                  children: [
-                    Container(
-                      width: Dimensions.listViewImg,
-                      height: Dimensions.listViewImg,
+        ListView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return Container(
+              margin: EdgeInsets.only(
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+                bottom: Dimensions.height10,
+              ),
+              child: Row(
+                children: [
+                  Container(
+                    width: Dimensions.listViewImg,
+                    height: Dimensions.listViewImg,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        Dimensions.radius20,
+                      ),
+                      color: Colors.white38,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage("asset/images/omg.png"),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Container(
+                      height: Dimensions.listViewTextContSize,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                          Dimensions.radius20,
+                        borderRadius: BorderRadius.only(
+                          topRight: Radius.circular(Dimensions.radius20),
+                          bottomRight: Radius.circular(Dimensions.radius20),
                         ),
-                        color: Colors.white38,
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage("asset/images/omg.png"),
+                        color: Colors.white,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.only(
+                          left: Dimensions.width10,
+                          right: Dimensions.width10,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            BigText(
+                              text: "Testing shit asf im bored",
+                            ),
+                            SizedBox(
+                              height: Dimensions.height10,
+                            ),
+                            SmallText(text: "Some mother fucker testing "),
+                            SizedBox(
+                              height: Dimensions.height10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                IconAndText(
+                                    icon: Icons.circle_sharp,
+                                    text: "Normal",
+                                    iconColor: AppColors.iconColor1),
+                                IconAndText(
+                                    icon: Icons.location_on,
+                                    text: "1.7 km",
+                                    iconColor: AppColors.mainColor),
+                                IconAndText(
+                                    icon: Icons.access_time_rounded,
+                                    text: "32 min",
+                                    iconColor: AppColors.iconColor2)
+                              ],
+                            ),
+                          ],
                         ),
                       ),
                     ),
-                    Expanded(
-                      child: Container(
-                        height: Dimensions.listViewTextContSize,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(Dimensions.radius20),
-                            bottomRight: Radius.circular(Dimensions.radius20),
-                          ),
-                          color: Colors.white,
-                        ),
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                            left: Dimensions.width10,
-                            right: Dimensions.width10,
-                          ),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              BigText(
-                                text: "Testing shit asf im bored",
-                              ),
-                              SizedBox(
-                                height: Dimensions.height10,
-                              ),
-                              SmallText(text: "Some mother fucker testing "),
-                              SizedBox(
-                                height: Dimensions.height10,
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceBetween,
-                                children: [
-                                  IconAndText(
-                                      icon: Icons.circle_sharp,
-                                      text: "Normal",
-                                      iconColor: AppColors.iconColor1),
-                                  IconAndText(
-                                      icon: Icons.location_on,
-                                      text: "1.7 km",
-                                      iconColor: AppColors.mainColor),
-                                  IconAndText(
-                                      icon: Icons.access_time_rounded,
-                                      text: "32 min",
-                                      iconColor: AppColors.iconColor2)
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              );
-            },
-          ),
-        )
+                  ),
+                ],
+              ),
+            );
+          },
+        ),
       ],
     );
   }
