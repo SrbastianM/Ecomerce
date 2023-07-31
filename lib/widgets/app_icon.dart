507669677283db796e15dd3798bdfc8b56c92bcd
Tenki <srodriguezdev.l@gmail.com>
2023-07-31 12:24:57 -0500

    Ajustamos el standar de los iconos a utilizar en la app

---
+++ lib/widgets/app_icon.dart
@@ -0,0 +1,33 @@
+import 'package:eccommerce/utils/dimensions.dart';
+import 'package:flutter/material.dart';
+
+class AppIcon extends StatelessWidget {
+  final IconData icon;
+  final Color backgroundColor;
+  final Color iconColor;
+  final double size;
+
+  AppIcon(
+      {super.key,
+      required this.icon,
+      this.backgroundColor = const Color(0xFFfcf4e4),
+      this.iconColor = const Color(0xFF756d54),
+      this.size = 40});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: size,
+      height: size,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(size / 2),
+        color: backgroundColor,
+      ),
+      child: Icon(
+        icon,
+        color: iconColor,
+        size: Dimensions.iconSize16,
+      ),
+    );
+  }
+}
