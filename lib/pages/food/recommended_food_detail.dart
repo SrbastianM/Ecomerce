a824cfc63c9dd60c9d58a17546ce3897fbab7a92
Tenki <srodriguezdev.l@gmail.com>
2023-08-09 10:22:07 -0500

    Se Culmina la creacion de la pagina recommended_food, y ahora se va a integrar las paginas creadas con el resto del app

--- lib/pages/food/recommended_food_detail.dart
+++ lib/pages/food/recommended_food_detail.dart
@@ -68,6 +68,95 @@ class RecommendedFoodDetail extends StatelessWidget {
           )
         ],
       ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Container(
+            padding: EdgeInsets.only(
+              left: Dimensions.width20 * 2.5,
+              right: Dimensions.width20 * 2.5,
+              top: Dimensions.height10,
+              bottom: Dimensions.height10,
+            ),
+            //margin: ,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                AppIcon(
+                    iconSize: Dimensions.iconSize24,
+                    iconColor: Colors.white,
+                    backgroundColor: AppColors.mainColor,
+                    icon: Icons.remove),
+                BigText(
+                  text: "\$12.88 " + " X " + " 0",
+                  color: AppColors.mainBlackColor,
+                  size: Dimensions.font26,
+                ),
+                AppIcon(
+                  iconSize: Dimensions.iconSize24,
+                  iconColor: Colors.white,
+                  backgroundColor: AppColors.mainColor,
+                  icon: Icons.add,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: Dimensions.bottomHeighBar,
+            padding: EdgeInsets.only(
+              top: Dimensions.height30,
+              bottom: Dimensions.height30,
+              left: Dimensions.width20,
+              right: Dimensions.width20,
+            ),
+            decoration: BoxDecoration(
+              color: AppColors.buttonBackgroundColor,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(Dimensions.radius20 * 2),
+                topRight: Radius.circular(Dimensions.radius20 * 2),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(
+                    top: Dimensions.height20,
+                    bottom: Dimensions.height20,
+                    left: Dimensions.width20,
+                    right: Dimensions.height20,
+                  ),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(
+                      Dimensions.radius20,
+                    ),
+                  ),
+                  child: Icon(
+                    Icons.favorite,
+                    color: AppColors.mainColor,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.only(
+                    top: Dimensions.height20,
+                    bottom: Dimensions.height20,
+                    left: Dimensions.width20,
+                    right: Dimensions.width20,
+                  ),
+                  child: BigText(
+                    text: "\$10 | Add to cart",
+                    color: Colors.white,
+                  ),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(Dimensions.radius20),
+                      color: AppColors.mainColor),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/app_icon.dart
+++ lib/widgets/app_icon.dart
@@ -6,13 +6,16 @@ class AppIcon extends StatelessWidget {
   final Color backgroundColor;
   final Color iconColor;
   final double size;
+  final double iconSize;
 
-  AppIcon(
-      {super.key,
-      required this.icon,
-      this.backgroundColor = const Color(0xFFfcf4e4),
-      this.iconColor = const Color(0xFF756d54),
-      this.size = 40});
+  AppIcon({
+    super.key,
+    required this.icon,
+    this.backgroundColor = const Color(0xFFfcf4e4),
+    this.iconColor = const Color(0xFF756d54),
+    this.size = 40,
+    this.iconSize = 16,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -26,7 +29,7 @@ class AppIcon extends StatelessWidget {
       child: Icon(
         icon,
         color: iconColor,
-        size: Dimensions.iconSize16,
+        size: iconSize,
       ),
     );
   }
